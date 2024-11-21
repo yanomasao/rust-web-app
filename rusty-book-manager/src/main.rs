39460f2f87569763828c0c63da6ebab8dd3a912b
Masao Yano <masao.yano@gmail.com>
2024-11-21 16:29:49 +0900

    コメント追加

--- rusty-book-manager/src/main.rs
+++ rusty-book-manager/src/main.rs
@@ -3,16 +3,19 @@ use std::net::{Ipv4Addr, SocketAddr};
 use axum::{routing::get, Router};
 use tokio::net::TcpListener;
 
-#[tokio::main]
+#[tokio::main] // main関数を非同期関数にする
 async fn main() {
-    println!("Hello, world!");
+    // ルータの生成
     let app = Router::new().route("/hello", get(hello_world));
+    // ポート設定
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    // サーバの起動
     let listener = TcpListener::bind(addr).await.unwrap();
     println!("Listening on {}", addr);
     axum::serve(listener, app).await.unwrap();
 }
 
+// ハンドラ
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
