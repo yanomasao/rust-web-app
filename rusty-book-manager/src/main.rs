6235253fe74f3ecbd2a53b77f1b9201a36e97715
Masao Yano <masao.yano@gmail.com>
2024-11-21 16:44:47 +0900

    anyhow導入

--- rusty-book-manager/src/main.rs
+++ rusty-book-manager/src/main.rs
@@ -1,21 +1,28 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
-use axum::{routing::get, Router};
+use anyhow::Result;
+use axum::{http::StatusCode, routing::get, Router};
 use tokio::net::TcpListener;
 
 #[tokio::main] // main関数を非同期関数にする
-async fn main() {
+async fn main() -> Result<()> {
     // ルータの生成
-    let app = Router::new().route("/hello", get(hello_world));
+    let app = Router::new()
+        .route("/hello", get(hello_world))
+        .route("/health", get(health_check));
     // ポート設定
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
     // サーバの起動
-    let listener = TcpListener::bind(addr).await.unwrap();
+    let listener = TcpListener::bind(addr).await?;
     println!("Listening on {}", addr);
-    axum::serve(listener, app).await.unwrap();
+    Ok(axum::serve(listener, app).await?)
 }
 
 // ハンドラ
 async fn hello_world() -> &'static str {
     "Hello, world!"
+}
+
+pub async fn health_check() -> StatusCode {
+    StatusCode::OK
 }
